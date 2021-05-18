b19a654ae1362666c67e3e893f7ad86876e19bb9
Grzechu <grzegorz.jamrooz@gmail.com>
2021-05-18 20:44:01 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# PHP Common
+
+## Description
+
+Collection of common interfaces shared between projects
+
+## Installation
+
+```
+composer require grzegorz-jamroz/php-common
+```
---
+++ src/ArrayConstructable.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ifrost\Common;
+
+interface ArrayConstructable
+{
+    /**
+     * @param array<string, mixed> $data
+     */
+    public static function createFromArray(array $data): self;
+}
