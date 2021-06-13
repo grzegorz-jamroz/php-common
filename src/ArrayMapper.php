3e67f0c4dee3a93aadfd7f9fdde04a58a04735bb
Grzechu <grzegorz.jamrooz@gmail.com>
2021-06-13 14:39:46 +0200

    added ArrayMapper

---
+++ src/ArrayMapper.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ifrost\Common;
+
+class ArrayMapper
+{
+    public static function orderBy(
+        array $array,
+        string $fieldName,
+        string $direction = 'ASC'
+    ): array {
+        uasort($array, function ($a, $b) use ($fieldName, $direction) {
+            $a = $a[$fieldName];
+            $b = $b[$fieldName];
+
+            if ($a === $b) {
+                return 0;
+            }
+
+            if ('DESC' === $direction) {
+                return ($a > $b) ? -1 : 1;
+            }
+
+            return ($a < $b) ? -1 : 1;
+        });
+
+        return $array;
+    }
+}
